73efbd65448321c395c3f8f76ea56e9074c31814
Jayden <uglydogisdog@outlook.com>
2020-05-14 18:16:57 +0800

    setting change

--- create_datalist.py
+++ create_datalist.py
@@ -3,17 +3,19 @@ import numpy as np
 
 
 def make_datalist(data_fd, data_list):
-    filename_all = os.listdir(data_fd)
-    filename_all = [data_fd+'/'+filename+'\n' for filename in filename_all if filename.endswith('.tfrecords')]
+    filename_all = os.listdir(data_fd)[:1000]
+    filename_all = [data_fd+'/'+filename +
+                    '\n' for filename in filename_all if filename.endswith('.tfrecords')]
 
     np.random.shuffle(filename_all)
-    np.random.shuffle(filename_all)
+
     with open(data_list, 'w') as fp:
         fp.writelines(filename_all)
 
 
 if __name__ == '__main__':
-    data_fd = './data/training_mr'
-    data_list = './data/datalist/training_mr.txt'
-
-    make_datalist(data_fd, data_list)
+    for run_type in ['training', 'validation']:
+        for data_type in ['ct', 'mr']:
+            data_fd = './data/' + run_type + '_' + data_type
+            data_list = './data/datalist/' + run_type + '_' + data_type + '.txt'
+            make_datalist(data_fd, data_list)
